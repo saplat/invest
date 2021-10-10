d57d7e504bd2eeed3ba1fcda85c9d800a2519004
GypsyBaby <2ssdmit@gmail.com>
2021-10-11 01:44:26 +0300

    'hyachechnaya'

--- taskmanager/main/urls.py
+++ taskmanager/main/urls.py
@@ -5,5 +5,5 @@ from . import views
 
 urlpatterns = [
     path('', views.index),
-    path('news', views.news)
+    path('news', views.news, name='news')
 ]
--- taskmanager/main/views.py
+++ taskmanager/main/views.py
@@ -1,11 +1,27 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-# Create your views here.
+import requests
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+apikey = os.getenv('apikey')
+
 
 #def news(request):
  #   return HttpResponse("<h4>То шо новости</h4>")
+
 def news(request):
-    return render(request, 'main/news.html')
+    url = f'https://newsapi.org/v2/top-headlines?country=us&category=business&apiKey={apikey}'
+    response = requests.get(url)
+    data = response.json()
+
+    articles = data['articles']
+
+    context = {
+        'articles':articles
+    }
+    return render(request, 'main/news.html', context)
 
 
 def index(request):
